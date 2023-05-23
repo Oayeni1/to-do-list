b4d4d1bfa86b29bda8275b453fb053b0cad90a54
Oayeni1 <Oayeni88@gmail.com>
2023-05-23 16:12:04 +0100

    goody commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Header from "./componenets/Header.js";
+import Form from "./componenets/Form.js";
+import TodosList from "./componenets/TodosList.js";
+import LogForm from "./componenets/logForm.js";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="app-wrapper">
+        <div>
+          <Header/>
+        </div>
+        <div>
+          <Form
+            input={input}
+            setInput={setInput}
+            todos={todos}
+            setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
+          />
+        </div>
+        <div>
+          <TodosList 
+          todos={todos} 
+          setTodos={setTodos} 
+          setEditTodo={setEditTodo}/>
+        </div>
+      </div>
+      <div>
+        <LogForm />
+      </div>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/componenets/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='head'>
+            <p>MY TO-DO LIST APPS</p>
+        </div>
+    )
+}
+
+export default Header;
